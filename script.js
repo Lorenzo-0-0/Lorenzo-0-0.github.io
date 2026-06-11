ce99d6895bacca2bb354172541ea1d5eef5f3ef9
Lorenzo-0-0 <jingliangli99@gmail.com>
2026-06-11 16:04:57 +0800

    feat: smaller visitor globe (200px); click opens site stats page in new tab

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ document.querySelectorAll('.pub-teaser--video').forEach(function (wrap) {
   });
 });
 
+
+// Visitor globe: send clicks to this site's public stats page in a new tab
+// (widget's injected anchor defaults to the mapmyvisitors.com homepage).
+(function () {
+  var STATS_URL = 'https://mapmyvisitors.com/web/1c5c2';
+  function fix() {
+    var a = document.getElementById('mmvst_a');
+    if (!a) return false;
+    if (a.getAttribute('href') !== STATS_URL) {
+      a.setAttribute('href', STATS_URL);
+      a.setAttribute('target', '_blank');
+      a.setAttribute('rel', 'noopener');
+    }
+    return true;
+  }
+  var globe = document.querySelector('.visitor-globe');
+  if (!globe) return;
+  fix();
+  new MutationObserver(fix).observe(globe, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['href']
+  });
+})();
