86787f620c501f04b1a3ad50fa6430d02c14c3bc
Lorenzo-0-0 <jingliangli99@gmail.com>
2026-06-16 21:36:12 +0800

    fix: globe uses correct domain-bound MapMyVisitors hash; link Bohan Hou; stats click -> own hits.sh

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.querySelectorAll('.pub-teaser--video').forEach(function (wrap) {
 // Visitor globe: send clicks to this site's public stats page in a new tab
 // (widget's injected anchor defaults to the mapmyvisitors.com homepage).
 (function () {
-  var STATS_URL = 'https://mapmyvisitors.com/web/1c5c2';
+  var STATS_URL = 'https://hits.sh/lorenzo-0-0.github.io/';
   function fix() {
     var a = document.getElementById('mmvst_a');
     if (!a) return false;
